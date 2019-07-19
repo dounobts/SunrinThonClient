449a295624e42c01529c8bf16157dc011f608485
WooLyung Lee <vlfkdydtk@naver.com>
2019-07-19 18:54:36 +0900

    add login layout

--- app/src/main/java/com/example/sunrinthonclient/Fragments/LoginFragment.java
+++ app/src/main/java/com/example/sunrinthonclient/Fragments/LoginFragment.java
@@ -1,22 +1,38 @@
 package com.example.sunrinthonclient.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.sunrinthonclient.R;
 
 import androidx.fragment.app.Fragment;
 
-public class LoginFragment extends Fragment {
+public class LoginFragment extends Fragment implements View.OnClickListener {
     View view;
+
+    TextView register;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        register = view.findViewById(R.id.button_register);
+
+        register.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.equals(register)) {
+
+        }
     }
 }
