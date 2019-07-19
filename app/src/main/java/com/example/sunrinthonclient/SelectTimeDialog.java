24ebed94086d9874e00a479f0bf60131b820eeb1
jinhyeokfang <jinhyeokfang@gmail.com>
2019-07-20 04:44:13 +0900

    add reserve

--- app/src/main/java/com/example/sunrinthonclient/SelectTimeDialog.java
+++ app/src/main/java/com/example/sunrinthonclient/SelectTimeDialog.java
@@ -3,6 +3,7 @@ package com.example.sunrinthonclient;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -11,10 +12,18 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.sunrinthonclient.Activities.MainActivity;
 import com.example.sunrinthonclient.Activities.ReservationActivity;
 import com.example.sunrinthonclient.Fragments.SelectDateBottomSheetFragment;
+import com.example.sunrinthonclient.Retrofit.Client;
+import com.google.android.material.snackbar.Snackbar;
 
 import org.w3c.dom.Text;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SelectTimeDialog extends Dialog {
     public SelectTimeDialog(final Context context, final SelectDateBottomSheetFragment fragment, final Activity activity) {
@@ -30,7 +39,27 @@ public class SelectTimeDialog extends Dialog {
             public void onClick(View view) {
                 Log.i("예약", "YES");
                 dismiss();
-                new CompleteDialog(context, fragment, activity).show();
+                Client.retrofitService.reserve(Client.username, SelectedData.place, SelectedData.month + 1, SelectedData.day, SelectedData.time). enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        switch(response.code()) {
+                            case 200:
+                                new CompleteDialog(context, fragment, activity).show();
+                                break;
+                            case 403:
+                                Snackbar.make(yes, "예약 실패 : 예약이 불가능한 공간입니다.", Snackbar.LENGTH_SHORT).show();
+                                break;
+                            case 500:
+                                Snackbar.make(yes, "예약 실패 : 서버 오류", Snackbar.LENGTH_SHORT).show();
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                    }
+                });
             }
         });
         no.setOnClickListener(new View.OnClickListener() {
