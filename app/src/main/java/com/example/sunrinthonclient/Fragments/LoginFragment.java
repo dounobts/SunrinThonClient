0ff9116fc9614b00246a73e951798748b5db016d
WooLyung Lee <vlfkdydtk@naver.com>
2019-07-19 13:11:08 +0900

    add login activity layout

---
+++ app/src/main/java/com/example/sunrinthonclient/Fragments/LoginFragment.java
@@ -0,0 +1,21 @@
+package com.example.sunrinthonclient.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sunrinthonclient.R;
+
+public class LoginFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/sunrinthonclient/Fragments/RegisterFragment.java
@@ -0,0 +1,23 @@
+package com.example.sunrinthonclient.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sunrinthonclient.R;
+
+public class RegisterFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        return view;
+    }
+}
